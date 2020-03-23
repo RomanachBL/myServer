4b183b52ea11c37e246edc653cb26c1921229e4f
RomanachBL <carrey.romain@gmail.com>
2020-03-23 18:38:23 +0100

    Initialisation serveur

--- myServer/src/com/cgi/Server.java
+++ myServer/src/com/cgi/Server.java
@@ -1,7 +1,21 @@
 package com.cgi;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.cgi.myCatalina.Handler;
+import com.sun.net.httpserver.HttpServer;
+
 public class Server {
-	public static void main(String[] args) {
-		System.out.println("Hello Main !");
+	public static void main(String[] args) throws IOException {
+		HttpServer myServer = HttpServer.create(new InetSocketAddress(8080), 0);
+		
+		// Créer le context (handler facultatif dans le deuxième argument).
+		myServer.createContext("/", new Handler());
+		// Permet de retourner un getExecutor si le setExecutor existe.
+		myServer.setExecutor(null);
+		
+		myServer.start();
+		System.out.println("Serveur en écoute sur le port "+ myServer.getAddress().getPort());
 	}
 }
